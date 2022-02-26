76e8878c7ec36888f94efd0c1f1024d7f2c34cc9
Samuel Cedarbaum <scedarbaum@gmail.com>
2022-02-26 18:28:36 -0500

    Remove unused variable in SubwayIcon.styles

--- src/SubwayIcon/SubwayIcon.styles.tsx
+++ src/SubwayIcon/SubwayIcon.styles.tsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
 
+/*
+    Styling based on https://github.com/CityOfNewYork/nyc-core-framework
+*/
+
 const ICON_PADDING_EM = 0.4
-//const TEXT_NUDGE_EM = 0.05
-const TEXT_NUDGE_EM = 0.0
 
 export interface SubwayIconContainerProps {
     clickable?: boolean
@@ -23,7 +25,6 @@ export const SubwayIconContainer = styled.div<SubwayIconContainerProps>(props =>
 
 export const BaseSubwayIcon = styled.span<SubwayIconColorProps>(props => ({
     position: 'relative',
-    paddingTop: `${TEXT_NUDGE_EM}em`,
     fontSize: '32px',
     zIndex: 5,
     height: '1em',
@@ -44,7 +45,7 @@ export const BaseSubwayIcon = styled.span<SubwayIconColorProps>(props => ({
         position: 'absolute',
         zIndex: -1,
         width: `calc(100% + ${ICON_PADDING_EM}em)`,
-        height: `calc(100% + ${ICON_PADDING_EM - TEXT_NUDGE_EM}em)`,
+        height: `calc(100% + ${ICON_PADDING_EM}em)`,
         left: `-${ICON_PADDING_EM / 2}em`,
         top: `-${ICON_PADDING_EM / 2}em`,
         borderRadius: '50%',
@@ -61,7 +62,7 @@ export const ExpressSubwayIcon = styled(BaseSubwayIcon)({
         transform: 'rotate(45deg)',
         borderRadius: 0,
         width: `calc(100% + ${ICON_PADDING_EM / 4}em)`,
-        height: `calc(100% + ${ICON_PADDING_EM / 4 - TEXT_NUDGE_EM}em)`,
+        height: `calc(100% + ${ICON_PADDING_EM / 4}em)`,
         left: `-${ICON_PADDING_EM / 8}em`,
         top: `-${ICON_PADDING_EM / 8}em`,
     }
