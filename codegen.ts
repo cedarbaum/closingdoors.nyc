df6b758df2936f31126e20f01e7939609dd2215d
Samuel Cedarbaum <scedarbaum@gmail.com>
2023-04-10 03:10:33 -0400

    Remove schema.json and use schema.graphql instead

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'graphql/schema.json',
+  schema: 'graphql/schema.graphql',
   documents: ['./graphql/*.graphql'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
