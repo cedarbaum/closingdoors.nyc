0b8898b0cd82b8be08f34697f6e0d3c0e646fe88
Samuel Cedarbaum <scedarbaum@gmail.com>
2023-08-22 17:38:21 -0400

    Add `ENABLE_RATE_LIMITING` env variable to toggle rate limiter

--- utils/RateLimiting.ts
+++ utils/RateLimiting.ts
@@ -6,19 +6,28 @@ import requestIp from "request-ip";
 const APP_NAME = "closingdoors.nyc";
 const GLOBAL_API_LIMIT_ID = `${APP_NAME}_GLOBAL_LIMIT`;
 
-const globalRequestLimit = parseInt(process.env.DAILY_API_LIMIT!);
-const globalRateLimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.fixedWindow(globalRequestLimit, "24 h"),
-});
+let globalRateLimit: Ratelimit | null = null;
+let ipLimit: Ratelimit | null = null;
 
-const perIpPerMinRequestLimit = parseInt(process.env.PER_IP_PER_MIN_LIMIT!);
-const ipLimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(perIpPerMinRequestLimit, "1 m"),
-});
+if (process.env.ENABLE_RATE_LIMITING === "true") {
+  const globalRequestLimit = parseInt(process.env.DAILY_API_LIMIT!);
+  globalRateLimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.fixedWindow(globalRequestLimit, "24 h"),
+  });
+
+  const perIpPerMinRequestLimit = parseInt(process.env.PER_IP_PER_MIN_LIMIT!);
+  ipLimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(perIpPerMinRequestLimit, "1 m"),
+  });
+}
 
 export default async function apiQuotaAvailable(req: NextApiRequest) {
+  if (globalRateLimit === null || ipLimit === null) {
+    throw new Error("Rate limiters not initialized");
+  }
+
   const { success: globalRateOk } = await globalRateLimit.limit(
     GLOBAL_API_LIMIT_ID
   );
