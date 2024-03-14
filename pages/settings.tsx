ddb61cf87c7baeb692dea461b9e43366b6f3cb43
Samuel Cedarbaum <scedarbaum@gmail.com>
2024-03-14 02:44:42 -0400

    Make TOS popup larger

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -254,7 +254,7 @@ export default function Settings({ content }: any) {
               </Drawer.Trigger>
               <Drawer.Portal>
                 <Drawer.Overlay className="fixed inset-0 bg-black/60" />
-                <Drawer.Content className="bg-black flex flex-col fixed bottom-0 left-0 mx-auto right-0 max-h-[67%] max-w-md">
+                <Drawer.Content className="bg-black flex flex-col fixed bottom-0 left-0 mx-auto right-0 max-h-[72%] max-w-md">
                   <div className="max-w-md prose text-white w-full mx-auto flex flex-col overflow-auto p-4">
                     <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
                     <div
